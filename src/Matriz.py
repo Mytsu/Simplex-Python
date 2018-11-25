6ac1ecc6d9ab0c4f89f1d0ec462e7d0d47a850b5
Mytsu <tlc.jooker@gmail.com>
2018-11-25 09:51:45 +0000

    add comentarios e .gitignore

--- src/Matriz.py
+++ src/Matriz.py
@@ -3,22 +3,19 @@ from copy import deepcopy
 
 class Matriz(object):
 
-    def __init__(self):
-        pass
-
     # retorna o tamanho das linhas da matriz
     # @param m1 number[] - matriz
-    def getM(self, m):
+    def get_m(self, m):
         return len(m)
 
     # retorna o tamanho das colunas da matriz (verifica apenas a primeira coluna)
     # @param m1 number[] - matriz
-    def getN(self, m):
+    def get_n(self, m):
         return len(m[0])
 
     # multiplica a matriz por um escalar, retorna a nova matriz sem alterar a anterior
     # @param m1 number[] - matriz
-    def multEscalar(self, m, e):
+    def multiplica_escalar(self, m, e):
         mat = deepcopy(m)
         for i in range(mat):
             for j in range(mat):
@@ -31,16 +28,16 @@ class Matriz(object):
     # @param v number[] - informa o vetor a ser multiplicado pelo vetor da matriz
     # @param n number - informa o índice do vetor da matriz
     # @param linha boolean - informa se deve buscar o vetor nas linhas ou colunas da matriz
-    def prodEscalar(self, m, v, n=0, linha=True):
+    def produto_escalar(self, m, v, n=0, linha=True):
         # recupera o vetor a ser multiplicado
         if linha:
             # se for um vetor linha, apenas copia o vetor da matriz
-            vet = deepcopy(self.matriz[n])
+            vet = deepcopy(m[n])
         else:
             # se for um vetor coluna, itera pelas linhas coletando os elementos do vetor
             vet = []
-            for i in range(self.matriz):
-                vet.append(self.matriz[i][n])
+            for i in range(m):
+                vet.append(m[i][n])
         for i in range(vet):
             vet[i] *= v[i]
         return vet
@@ -48,7 +45,7 @@ class Matriz(object):
     # multiplica uma matriz m pela matriz na estrutura
     # @param m1 number[] - matriz 1
     # @param m2 number[] - matriz 2
-    def prodMatricial(self, m1, m2):
+    def produto_matricial(self, m1, m2):
         linhas1, colunas1 = (len(m1[0], len(m1)))
         linhas2, colunas2 = (len(m2[0], len(m2)))
         assert linhas1 == colunas2
@@ -61,6 +58,8 @@ class Matriz(object):
                     r[i][j] += m1[i][k] * m2[k][j]
         return r
 
+    # transpoe a matriz dada como entrada
+    # @param m number[] - matriz
     def transposta(self, m):
         assert len(m) == len(m[0])
         r = []
